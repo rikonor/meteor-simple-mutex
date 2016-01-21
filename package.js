0e0272c0ae97983ed3eda99c7027f2e31d274b6a
Or Rikon <rikonor@gmail.com>
2016-01-20 22:34:43 -0500

    Validate on remove as well on client and server

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rikonor:simple-mutex',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Lock Objects',
   git: 'https://www.github.com/rikonor/meteor-simple-mutex',
   documentation: 'README.md'
@@ -10,8 +10,8 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use([
     'ecmascript',
-    'dburles:collection-helpers',
-    'dburles:mongo-collection-instances'
+    'dburles:collection-helpers@1.0.4',
+    'dburles:mongo-collection-instances@0.3.5'
   ]);
   api.addFiles('lib/simple-mutex.js');
 });
